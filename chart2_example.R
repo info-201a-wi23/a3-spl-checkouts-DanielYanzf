9916381eabc42a3ff7fc215079a3eeb2815426cc
Daniel Yan <104042353+DanielYanzf@users.noreply.github.com>
2023-03-03 22:20:12 -0800

    Update chart2_example.R

--- chart2_example.R
+++ chart2_example.R
@@ -1,6 +1,10 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
+# Aggregate data by CheckoutYear and MaterialType
+data$CheckoutYear <- as.numeric(data$CheckoutYear)
+data$Checkouts <- as.numeric(data$Checkouts)
+yearly_material_checkouts <- aggregate(Checkouts ~ CheckoutYear + MaterialType, data, sum)
 
-library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+# Create bar chart with MaterialType on x-axis, Checkouts on y-axis, and year as fill
+ggplot(yearly_material_checkouts, aes(x = MaterialType, y = Checkouts, fill = factor(CheckoutYear))) +
+  geom_col(position = "dodge") +
+  labs(title = "Total Checkouts by Material Type and Year", x = "Material Type", y = "Total Checkouts") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
